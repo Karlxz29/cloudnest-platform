ff00be27cfe10403393d8dd42fd5450bda07d681
dkdk29 <20127211@student.hcmus.edu.vn>
2026-02-26 21:57:57 +0700

    Initial CloudNest commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const os = require("os");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>CloudNest Platform</h1>
+    <p>Server Hostname: ${os.hostname()}</p>
+    <p>Environment: Production</p>
+  `);
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(4000, () => {
+  console.log("CloudNest running on port 4000");
+});
